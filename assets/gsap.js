27af3be27f593519735001c2c7b06733a68a64b2
hiroshi <h.kawaguchi0204@gmail.com>
2023-10-29 06:55:24 +0900

    2023-10-29

---
+++ assets/gsap.js
@@ -0,0 +1,69 @@
+
+            gsap.from(".js__fade-in01",1.5,{
+        opacity:0,
+        y: 100,
+        scrollTrigger: {
+          trigger: ".js__fade-in01", // アニメーション開始のトリガー要素
+          start: "top center", // アニメーション開始位置
+        //   markers: true,
+        },
+      });
+      
+    gsap.from(".js__fade-in02",1.5,{
+        opacity:0,
+        y: 100,
+        scrollTrigger: {
+          trigger: ".section02", // アニメーション開始のトリガー要素
+          start: "top center", // アニメーション開始位置
+        },
+      });
+    gsap.from(".js__fade-in03",1.5,{
+        opacity:0,
+        y: 100,
+        scrollTrigger: {
+          trigger: ".js__fade-in03", // アニメーション開始のトリガー要素
+          start: "top center", // アニメーション開始位置
+        },
+      });
+    gsap.from(".js__fade-in04",1.5,{
+        opacity:0,
+        y: 100,
+        scrollTrigger: {
+          trigger: ".js__fade-in04", // アニメーション開始のトリガー要素
+          start: "top center", // アニメーション開始位置
+        },
+      });
+    gsap.from(".js__fade-in05",1.5,{
+        opacity:0,
+        y: 100,
+        scrollTrigger: {
+          trigger: ".js__fade-in05", // アニメーション開始のトリガー要素
+          start: "top center", // アニメーション開始位置
+        },
+      });
+    
+    
+    gsap.from(".js-fade-left01",1.5,{
+        opacity:0,
+        x: 10,
+        scrollTrigger: {
+          trigger: ".js-fade-left01", // アニメーション開始のトリガー要素
+          start: "top center", // アニメーション開始位置
+        },
+      });
+    gsap.from(".js-fade-right",1.5,{
+        opacity:0,
+        x: -10,
+        scrollTrigger: {
+          trigger: ".js-fade-right", // アニメーション開始のトリガー要素
+          start: "top center", // アニメーション開始位置
+        },
+      });
+    gsap.from(".js-fade-left02",1.5,{
+        opacity:0,
+        x:  10,
+        scrollTrigger: {
+          trigger: ".js-fade-left02", // アニメーション開始のトリガー要素
+          start: "top center", // アニメーション開始位置
+        },
+      });
