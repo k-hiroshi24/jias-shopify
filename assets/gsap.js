7d141947720f97721ad269b760a76fb4109a7edf
hiroshi <h.kawaguchi0204@gmail.com>
2023-10-31 06:34:33 +0900

    2023-10-30LP修正

--- assets/gsap.js
+++ assets/gsap.js
@@ -1,69 +1,76 @@
+gsap.from(".js__fade-in01",1,{
+  opacity:0,
+  y: 20,
+  scrollTrigger: {
+    trigger: ".js__fade-in01", // アニメーション開始のトリガー要素
+    start: "top 80%", // アニメーション開始位置
+    end:"bottom 80%",
+    // markers: true,
+  },
+});
 
-        gsap.from(".js__fade-in01",1,{
-        opacity:0,
-        y: 50,
-        scrollTrigger: {
-          trigger: ".js__fade-in01", // アニメーション開始のトリガー要素
-          start: "top center", // アニメーション開始位置
-        //   markers: true,
-        },
-      });
-      
-    gsap.from(".js__fade-in02",1,{
-        opacity:0,
-        y: 50,
-        scrollTrigger: {
-          trigger: ".section02", // アニメーション開始のトリガー要素
-          start: "top center", // アニメーション開始位置
-        },
-      });
-    gsap.from(".js__fade-in03",1,{
-        opacity:0,
-        y: 50,
-        scrollTrigger: {
-          trigger: ".js__fade-in03", // アニメーション開始のトリガー要素
-          start: "top center", // アニメーション開始位置
-        },
-      });
-    gsap.from(".js__fade-in04",1,{
-        opacity:0,
-        y: 50,
-        scrollTrigger: {
-          trigger: ".js__fade-in04", // アニメーション開始のトリガー要素
-          start: "top center", // アニメーション開始位置
-        },
-      });
-    gsap.from(".js__fade-in05",1,{
-        opacity:0,
-        y: 50,
-        scrollTrigger: {
-          trigger: ".js__fade-in05", // アニメーション開始のトリガー要素
-          start: "top center", // アニメーション開始位置
-        },
-      });
-    
-    
-    gsap.from(".js-fade-left01",1,{
-        opacity:0,
-        x: 10,
-        scrollTrigger: {
-          trigger: ".js-fade-left01", // アニメーション開始のトリガー要素
-          start: "top center", // アニメーション開始位置
-        },
-      });
-    gsap.from(".js-fade-right",1,{
-        opacity:0,
-        x: -10,
-        scrollTrigger: {
-          trigger: ".js-fade-right", // アニメーション開始のトリガー要素
-          start: "top center", // アニメーション開始位置
-        },
-      });
-    gsap.from(".js-fade-left02",1,{
-        opacity:0,
-        x:  10,
-        scrollTrigger: {
-          trigger: ".js-fade-left02", // アニメーション開始のトリガー要素
-          start: "top center", // アニメーション開始位置
-        },
-      });
+gsap.from(".js__fade-in02",1,{
+  opacity:0,
+  y: 20,
+  scrollTrigger: {
+    trigger: ".section02", // アニメーション開始のトリガー要素
+    start: "top 80%", // アニメーション開始位置
+    end:"bottom 80%",
+  },
+});
+gsap.from(".js__fade-in03",1,{
+  opacity:0,
+  y: 20,
+  scrollTrigger: {
+    trigger: ".js__fade-in03", // アニメーション開始のトリガー要素
+    start: "top 80%", // アニメーション開始位置
+    end:"bottom 80%",
+  },
+});
+gsap.from(".js__fade-in04",1,{
+  opacity:0,
+  y: 20,
+  scrollTrigger: {
+    trigger: ".js__fade-in04", // アニメーション開始のトリガー要素
+    start: "top 80%", // アニメーション開始位置
+    end:"bottom 80%",
+  },
+});
+gsap.from(".js__fade-in05",1,{
+  opacity:0,
+  y: 20,
+  scrollTrigger: {
+    trigger: ".js__fade-in05", // アニメーション開始のトリガー要素
+    start: "top 80%", // アニメーション開始位置
+    end:"bottom 80%",
+  },
+});
+
+
+gsap.from(".js-fade-left01",1,{
+  opacity:0,
+  x: 10,
+  scrollTrigger: {
+    trigger: ".js-fade-left01", // アニメーション開始のトリガー要素
+    start: "top center", // アニメーション開始位置
+  },
+});
+gsap.from(".js-fade-right",1,{
+  opacity:0,
+  x: -10,
+  scrollTrigger: {
+    trigger: ".js-fade-right", // アニメーション開始のトリガー要素
+    start: "top 80%", // アニメーション開始位置
+    end:"bottom 80%",
+  },
+});
+gsap.from(".js-fade-left02",1,{
+  opacity:0,
+  x:  10,
+  scrollTrigger: {
+    trigger: ".js-fade-left02", // アニメーション開始のトリガー要素
+    start: "top 80%", // アニメーション開始位置
+    end:"bottom 80%",
+  },
+});
+
